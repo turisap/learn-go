7dedb44ea7d27245c803e820aefa833304ed4c13
Kirill Shakirov <kirillshakirov57@gmail.com>
2023-12-16 16:42:25 +0700

    task: add mutexes

--- concurency/data-race.go
+++ concurency/data-race.go
@@ -7,23 +7,31 @@ import (
 )
 
 func main() {
+	t1 := time.Now()
 
 	const gr = 100
 	var wg sync.WaitGroup
 
 	wg.Add(gr * 2)
 
-	var n int = 1000
+	var n int = 0
+	var m sync.Mutex
 
 	for i := 0; i < 100; i++ {
+		// Mutexes
 		go func() {
 			time.Sleep(time.Second / 10)
+			m.Lock()
 			n++
+			m.Unlock()
 			wg.Done()
 		}()
 
 		go func() {
 			time.Sleep(time.Second / 10)
+			// another option
+			m.Lock()
+			defer m.Unlock()
 			n--
 			wg.Done()
 		}()
@@ -32,4 +40,5 @@ func main() {
 	wg.Wait()
 
 	fmt.Println("The final n value is: ", n)
+	fmt.Println("Time finished", time.Now().Sub(t1).Milliseconds(), "ms")
 }
