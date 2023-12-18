98da93f663bffc57b1c28cc6bf64ed1180e44e8b
Kirill Shakirov <kirillshakirov57@gmail.com>
2023-12-18 18:17:20 +0700

    task: add modules

---
+++ modules/go.mod
@@ -0,0 +1,5 @@
+module hello
+
+go 1.21.5
+
+require github.com/ddadumitrescu/hellomod v1.0.0 // indirect
---
+++ modules/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/ddadumitrescu/hellomod"
+
+func main() {
+	hellomod.SayHello()
+}
